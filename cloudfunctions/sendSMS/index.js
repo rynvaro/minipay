77a66f595411979b77d9e6674ea0b2a494e1d5e0
you <you@local.com>
2020-08-17 06:06:30 +0800

    add send sms cloudfunction

---
+++ cloudfunctions/sendSMS/index.js
@@ -0,0 +1,57 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+const db = cloud.database()
+
+const Core = require('@alicloud/pop-core');
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+
+    let code = Math.random().toString().substr(2,6)
+
+    var params = {
+        "RegionId": "cn-hangzhou",
+        "PhoneNumberJson": "[\""+event.phone+"\"]",
+        "SignNameJson": "[\"测试\"]",
+        "TemplateCode": "SMS_12680099",
+        "TemplateParamJson": "[{\"code\":\""+code+"\"}]"
+      }
+
+    var client = new Core({
+        accessKeyId: 'LTAI4FqBJQvDkWmw81T2scxy',
+        accessKeySecret: 'r5ZeEbvMz0lLM8tYweQNt0svm25b3b',
+        endpoint: 'https://dysmsapi.aliyuncs.com',
+        apiVersion: '2017-05-25'
+    });
+
+    var requestOption = {
+        method: 'POST'
+    };
+      
+    await client.request('SendBatchSms', params, requestOption).then((result) => {
+        console.log(result)
+    }, (ex) => {
+        console.log(ex);
+        throw(ex)
+    })
+
+    var result = await db.collection('codes').add({
+        data: {
+            phone: event.phone,
+            code: code,
+            time: Date.parse(new Date()),
+        },
+        success: res => {
+          console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+        },
+        fail: err => {
+          console.error('[数据库] [新增记录] 失败：', err)
+          throw(err)
+        }
+    })
+
+    return result
+}
