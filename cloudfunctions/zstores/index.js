06b939f0536bb79326265cbaa9a13b4093d8969c
you <you@local.com>
2020-09-07 04:02:59 +0800

    update

--- cloudfunctions/zstores/index.js
+++ cloudfunctions/zstores/index.js
@@ -1,11 +1,18 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
+cloud.init({env: cloud.DYNAMIC_CURRENT_ENV})
+
+const db = cloud.database({env: cloud.DYNAMIC_CURRENT_ENV})
 
 // 云函数入口函数
 exports.main = async (event, context) => {
     const wxContext = cloud.getWXContext()
+
+    let q = event.q
+    let type = event.type
+    let lat = event.lat
+    let lon = event.lon
 
     return {
         event,
