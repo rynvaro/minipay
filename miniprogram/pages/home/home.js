a7f97024ca8e2946688ac4ad1c5cf0326fcad049
ktlshya <ktlshya@sina.com>
2020-09-07 09:52:48 +0800

    update

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -49,6 +49,12 @@ Page({
     })
   },
 
+  scanpay: function(e) {
+    wx.navigateTo({
+      url: '../scanpay/scanpay?merchantID=' + e.currentTarget.dataset.id,
+    })
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
