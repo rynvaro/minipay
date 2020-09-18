f0749c2a88a3ef4ce44afe666a680c89cc1e78b6
you <you@local.com>
2020-09-18 14:29:21 +0800

    update

--- cloudfunctions/storeSetting/index.js
+++ cloudfunctions/storeSetting/index.js
@@ -24,30 +24,53 @@ exports.main = async (event, context) => {
         return -1
     }
 
-    await db.collection('mstores').doc(event.id).update({
-        data: {
-            address: event.address,
-            startTime: event.startTime,
-            endTime: event.endTime,
-            storeDesc: event.storeDesc,
-            storeType: parseInt(event.storeType),
-            geoPoint: db.Geo.Point(event.longitude, event.latitude),
-            longitude: event.longitude,
-            latitude: event.latitude,
-            merchantBankCard: event.merchantBankCard,
-            merchantPhone: event.merchantPhone,
-            storeName: event.storeName,
-            openid: wxContext.OPENID,
-            updatedAt: Date.parse(new Date()),
-        },
-        success: res => {
-            console.log('[数据库] [更新记录] 成功，记录 _id: ', res._id)
-        },
-        fail: err => {
-            console.error('[数据库] [更新记录] 失败：', err)
-            throw("update error")
-        }
-    })
+    let type = event.type 
+    delete event.type
+
+    if (type == 'merchant') {
+        await db.collection('mstores').doc(event.id).update({
+            data: {
+                merchantName: event.merchantName,
+                merchantBankCard: event.merchantBankCard,
+                merchantPhone: event.merchantPhone,
+                bank: event.bank,
+                openid: wxContext.OPENID,
+                complete: true,
+                updatedAt: Date.parse(new Date()),
+            },
+            success: res => {
+                console.log('[数据库] [更新记录] 成功，记录 _id: ', res._id)
+            },
+            fail: err => {
+                console.error('[数据库] [更新记录] 失败：', err)
+                throw("update error")
+            }
+        })
+    }else if (type == 'store'){
+        await db.collection('mstores').doc(event.id).update({
+            data: {
+                address: event.address,
+                startTime: event.startTime,
+                endTime: event.endTime,
+                storeDesc: event.storeDesc,
+                storeType: parseInt(event.storeType),
+                storeTypeOne: parseInt(event.storeTypeOne),
+                geoPoint: db.Geo.Point(event.longitude, event.latitude),
+                longitude: event.longitude,
+                latitude: event.latitude,
+                storeName: event.storeName,
+                openid: wxContext.OPENID,
+                updatedAt: Date.parse(new Date()),
+            },
+            success: res => {
+                console.log('[数据库] [更新记录] 成功，记录 _id: ', res._id)
+            },
+            fail: err => {
+                console.error('[数据库] [更新记录] 失败：', err)
+                throw("update error")
+            }
+        })
+    }
 
   return {}
 }
