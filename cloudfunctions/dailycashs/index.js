48fc167230bf1e46f58768e5a7c24b4c5ad9a759
ktlshya <ktlshya@sina.com>
2020-09-22 17:41:49 +0800

    add dailycashes triggers

---
+++ cloudfunctions/dailycashs/index.js
@@ -0,0 +1,20 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({
+    env: cloud.DYNAMIC_CURRENT_ENV
+})
+
+const db = cloud.database({env: cloud.DYNAMIC_CURRENT_ENV})
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+
+    console.log(new Date())
+
+    // await db.collection('cashflows').add({
+    //     data: new Date(),
+    // })
+
+    return {}
+}
